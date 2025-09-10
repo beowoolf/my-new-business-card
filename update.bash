b88625e9b65d9245db1fefb1a8daf673534c31ef
Paweł Wilczek <5646246+beowoolf@users.noreply.github.com>
2025-09-10 17:18:24 +0200

    Upgrade used Node.js version

--- update.bash
+++ update.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # mkdir -p ~/bin && ln -fs /usr/local/bin/node16 ~/bin/node && ln -fs /usr/local/bin/npm16 ~/bin/npm && source $HOME/.bash_profile
-mkdir -p ~/bin && ln -fs /usr/local/bin/node18 ~/bin/node && ln -fs /usr/local/bin/npm18 ~/bin/npm && source $HOME/.bash_profile
+# mkdir -p ~/bin && ln -fs /usr/local/bin/node18 ~/bin/node && ln -fs /usr/local/bin/npm18 ~/bin/npm && source $HOME/.bash_profile
+mkdir -p ~/bin && ln -fs /usr/local/bin/node20 ~/bin/node && ln -fs /usr/local/bin/npm20 ~/bin/npm && source $HOME/.bash_profile
 git pull && cd ~/cloned/pawel-wilczek.mojezapiski.pl/pw-frontend && yarn && yarn build && devil www restart pawel-wilczek.mojezapiski.pl
